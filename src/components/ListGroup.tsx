15bc0a12b828ecb75f2a15983ce1913554ca7b1a
cubeDHS2017 <152009466+cubeDHS2017@users.noreply.github.com>
2024-02-20 20:16:04 -0500

    Adding a if statement if there are no list items.

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -3,6 +3,9 @@ import { Fragment } from "react";
 function ListGroup() {
   const items = ["New York", "Tokyo", "London", "Paris"];
 
+  if (items.length === 0)
+    return <p>No items found</p>;
+
   return (
     <Fragment>
       <h1>List</h1>
